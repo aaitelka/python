3464d46a1b0dbf3ee23ec0e8031fc13108920662
aaitelka <aaitelka@gmail.com>
2026-01-08 20:27:09 +0000

    fix(flake8): fix files norm and  remove function calls.

--- module00/ex2/ft_harvest_total.py
+++ module00/ex2/ft_harvest_total.py
@@ -1,8 +1,5 @@
-
 def ft_harvest_total():
-    weight: num = 0
+    weight = 0
     for i in range(3):
         weight += int(input(f"Day {i + 1} harvest: "))
     print(f"Total harvest: {weight}")
-
-ft_harvest_total()
