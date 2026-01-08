3b93018b1ff1b7750f7f2302b6a436f71d344f77
aaitelka <aaitelka@gmail.com>
2026-01-08 20:20:22 +0000

    create `ft_plant_age.py` file and implement `ft_plant_age()` function on it.

---
+++ module00/ex3/ft_plant_age.py
@@ -0,0 +1,9 @@
+
+def ft_plant_age():
+    plante_age = int(input("Enter plant age in days: "))
+    if plante_age > 60:
+        print("Plant is ready to harvest!")
+    else:
+        print("Plant needs more time to grow.")
+
+ft_plant_age()
